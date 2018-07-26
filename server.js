3b246bc0524a175f6ba9b3517b8d629a17146927
anoop <anoopm6@gmail.com>
2018-07-26 13:20:32 +0530

    update

--- server.js
+++ server.js
@@ -8,6 +8,35 @@ port = process.env.PORT || 3000,
 app = express();
 let alexaVerifier = require('alexa-verifier');
 
+const APP_ID = 'amzn1.ask.skill.2b2693e0-b306-49b4-aaae-fa68bd270ce4';
+
+const SKILL_NAME = 'Marvel Heroes';
+const GET_HERO_MESSAGE = "Here's your hero: ";
+const HELP_MESSAGE = 'You can say tell me the name of a hero, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Enjoy the day...Goodbye!';
+const MORE_MESSAGE = 'Do you want more?'
+const PAUSE = '<break time="0.3s" />'
+const WHISPER = '<amazon:effect name="whispered"/>'
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
+//=========================================================================================================================================
+const data = [
+    'Black Panther  ',
+    'She-Hulk ',
+    'Thor',
+    'Nova ',
+    'Doctor Strange ',
+    'Hawkeye',
+    'Quicksilver ',
+    'Blade',
+    'Black Widow',
+    'Captain Marvel',
+    'Deadpool',
+    'Elektra',
+    'Shaktimaan',
+];
 
 // var privateKey = fs.readFileSync('key.pem', 'utf8');
 // var certificate = fs.readFileSync('cert.pem', 'utf8');
@@ -44,16 +73,7 @@ function requestVerifier(req, res, next) {
 }
 app.post('/marvelheroes', requestVerifier, function(req, res) {
   if (req.body.request.type === 'LaunchRequest') {
-    res.json({
-      "version": "1.0",
-      "response": {
-        "shouldEndSession": true,
-        "outputSpeech": {
-          "type": "SSML",
-          "ssml": "<speak>Hmm <break time=\"1s\"/> What day do you want to know about?</speak>"
-        }
-      }
-    });
+    res.json(getNewHero());
   }
   else if (req.body.request.type === 'SessionEndedRequest') { /* ... */ }
   else if (req.body.request.type === 'IntentRequest' &&
@@ -82,6 +102,30 @@ app.post('/marvelheroes', requestVerifier, function(req, res) {
     }
   });
 
+function getNewHero(){
+
+    const welcomeSpeechOutput = 'Welcom to marvel heroes, here is your hero<break time="0.3s" />'
+    const heroArr = data;
+const heroIndex = Math.floor(Math.random() * heroArr.length);
+const randomHero = heroArr[heroIndex];
+const tempOutput = WHISPER + GET_HERO_MESSAGE + randomHero + PAUSE;
+const speechOutput = "<speak>" + welcomeSpeechOutput + tempOutput + MORE_MESSAGE + "</speak>"
+const more = MORE_MESSAGE
+
+var jsonObj = {
+  "version": "1.0",
+  "response": {
+    "shouldEndSession": true,
+    "outputSpeech": {
+      "type": "SSML",
+      "ssml": speechOutput
+    }
+  }
+}
+
+return jsonObj;
+
+}
   //httpServer.listen(port);
   //httpsServer.listen(7000);
   app.listen(port);
